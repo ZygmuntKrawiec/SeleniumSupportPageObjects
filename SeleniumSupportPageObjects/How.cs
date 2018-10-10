2d8b3f89aa2008a1eecb8900a9352de790a230e8
Zygmunt Krawiec <zygmuntkrawiec@gmail.com>
2018-10-11 01:01:39 +0200

    Add How class

---
+++ SeleniumSupportPageObjects/How.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumSupportPageObjects
+{
+    public enum How
+    {        
+        Id = 0,        
+        Name = 1,       
+        TagName = 2,       
+        ClassName = 3,       
+        CssSelector = 4,        
+        LinkText = 5,        
+        PartialLinkText = 6,        
+        XPath = 7,        
+        Custom = 8
+    }
+}
