db998b716c59866757ab1e478af60248be334db9
Zygmunt Krawiec <zygmuntkrawiec@gmail.com>
2018-10-10 15:17:57 +0200

    Add PageFactory class

---
+++ SeleniumSupportPageObjects/PageFactory.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumSupportPageObjects
+{
+    public static class PageFactory
+    {
+        public void InitElements(ISearchContext driver, object page)
+        {
+        }
+    }
+}
