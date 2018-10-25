de94d864ff7798cfcd405b84c581a9337ab08faa
Zygmunt Krawiec <zygmuntkrawiec@gmail.com>
2018-10-25 15:02:28 +0200

    Set configurations to Test project

---
+++ SeleniumSupportPageObjects.NUnit.Tests/PageFactoryConfig/PageStub.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumSupportPageObjects.NUnit.Tests.PageFactoryConfig
+{
+    class PageStub
+    {
+        [FindsBy(How = How.Id, Using = "")]
+        public IWebElement IWebElement { get; set; }
+    }
+}
---
+++ SeleniumSupportPageObjects.NUnit.Tests/PageFactoryConfig/WebElementStub.cs
@@ -0,0 +1,120 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Drawing;
+
+namespace SeleniumSupportPageObjects.NUnit.Tests.PageFactoryConfig
+{
+    class WebElementStub : IWebElement
+    {
+        string _text;
+        public WebElementStub(string text)
+        {
+            _text = text;
+        }
+        public bool Displayed
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool Enabled
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Point Location
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool Selected
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Size Size
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string TagName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Click()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IWebElement FindElement(By by)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReadOnlyCollection<IWebElement> FindElements(By by)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAttribute(string attributeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCssValue(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetProperty(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendKeys(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Submit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
