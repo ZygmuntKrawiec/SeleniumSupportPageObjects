9b043cbef7bc10f132de20919381cd0ca258191b
Zygmunt Krawiec <zygmuntkrawiec@gmail.com>
2018-11-03 23:42:57 +0100

    Add fields to POMTypeClass

--- SeleniumSupportPageObjects/PageObjectModel/POMTypeCreator.cs
+++ SeleniumSupportPageObjects/PageObjectModel/POMTypeCreator.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +10,17 @@ namespace SeleniumSupportPageObjects.PageObjectModel
 {
     public class POMTypeCreator : IDisposable
     {
+        private static AssemblyName _assemblyName;
+        private static AssemblyBuilder _assemblyBuilder;
+        private static ModuleBuilder _moduleBuilder;
+        private static List<Type> _listOfAllTypes;
+
+        private TypeBuilder _typeBulider;
+        private ConstructorBuilder _ctor0;
+        private Type _createdType;
+        private List<PropertiesFeaturesContainer> _listOfProperties;
+
+
         public void Dispose()
         {
             throw new NotImplementedException();
